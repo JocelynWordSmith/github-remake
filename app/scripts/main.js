85a7c9bf87ef9bc42b696cf2aad8a041ea3f9dbb
joshuawordsmith <Joshua Smith>
2014-05-19 03:39:15 -0400

    all elements in rough places

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,22 +1,40 @@
 'use strict';
-//grabs the profile info
-$.getJSON('https://api.github.com/users/joshuawordsmith?client_id=567f27a0a07d1b816db5&client_secret=2076ba9086dc6126360d7ae90e0c062bb951542d').done(function(data){
-	console.log(data);
-});
+var user = 'JoshuaWordSmith';
 
 
-var showRepos = _.template($('.repo-patriarch').text()); 
 
 
-function renderRepos (array){
+
+//puts info into repo-list via the repo master script
+var showInfo = _.template($('.info-master').text());
+//defines renderObj function
+function renderObj (data, destination){
+		var rendered = showInfo(data);
+		destination.prepend(rendered);
+	}
+
+//puts info into repo-list via the repo master script
+var showRepos = _.template($('.repo-master').text());
+//defines renderArr function
+function renderArr (array, destination){
 	array.forEach(function(module){
 		var rendered = showRepos(module);
-		$('.repo-list').prepend(rendered);
+		destination.prepend(rendered);
 	});
 }
 
+
+
+//grabs the profile info
+$.getJSON('https://api.github.com/users/'+ user + '?client_id=567f27a0a07d1b816db5&client_secret=2076ba9086dc6126360d7ae90e0c062bb951542d').done(function(data){
+	renderObj(data, $('.info-list'));
+});
+
+
+
+
 //grabs the repo info
-$.getJSON('https://api.github.com/users/JoshuaWordSmith/repos?client_id=567f27a0a07d1b816db5&client_secret=2076ba9086dc6126360d7ae90e0c062bb951542d').done(function(data){
-	renderRepos(data);
+$.getJSON('https://api.github.com/users/' + user + '/repos?client_id=567f27a0a07d1b816db5&client_secret=2076ba9086dc6126360d7ae90e0c062bb951542d').done(function(data){
+	renderArr(data, $('.repo-list'));
 });
 
